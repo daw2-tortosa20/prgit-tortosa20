a0810d333ac6bdad8f93db87058cf6465bdbdbff
prgit-tortosa20 <cristian.tortosa.ovgithub@gmail.com>
2019-10-05 00:10:15 +0200

    $git commit -m "prgit-tortosa20 v2"

--- mitja.c
+++ mitja.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 int main() {
@@ -13,7 +14,7 @@ int main() {
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
 		printf("Vols continuar (s/n): ");
-		scanf(" %c",&consulta); 
+		scanf("%s",&consulta);
 	}
 	mitja=suma/numero_repeticions;
 	printf("La mitja és: %.2f\n",mitja);	
